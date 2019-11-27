00c69245b121eb9e4cc659bd704cf1e55262c9aa
김재우 <rlawodn102@naver.com>
2019-11-28 04:44:43 +0900

    added print line

--- Git Assignment/src/Hard_Git_Task.java
+++ Git Assignment/src/Hard_Git_Task.java
@@ -2,8 +2,8 @@
 public class Hard_Git_Task {
 
 	public static void main(String[] args) {
-		System.out.println("I'm already tired");
-
+		System.out.println("I'm already tired.");
+		System.out.println("Have a long way to go.");
 	}
 
 }
