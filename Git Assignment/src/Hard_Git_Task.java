ea5b3362b35b597c3e0b201b7e0413ac23adaae5
김재우 <rlawodn102@naver.com>
2019-11-28 04:54:36 +0900

    coolfix branch change a code

--- Git Assignment/src/Hard_Git_Task.java
+++ Git Assignment/src/Hard_Git_Task.java
@@ -4,6 +4,7 @@ public class Hard_Git_Task {
 	public static void main(String[] args) {
 		System.out.println("I'm already tired.");
 		System.out.println("Have a long way to go.");
+		System.out.println("a third now");
 	}
 
 }
