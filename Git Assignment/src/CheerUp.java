b3d4276bcb530d55b42db1f0992e44e29d90c2aa
rlawodn102 <55470184+rlawodn102@users.noreply.github.com>
2019-11-30 00:58:58 +0900

    GitHub for git pull

--- Git Assignment/src/CheerUp.java
+++ Git Assignment/src/CheerUp.java
@@ -5,6 +5,7 @@ public class CheerUp {
 		System.out.println("almost done.");
 		System.out.println("When will it end?");
 		System.out.println("The end is visible.");
+		System.out.println("The last real.");
 
 	}
 
